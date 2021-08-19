a68dd20c6de3db44252bd0e2059f51db705a770c
Jimmy Li <jimmyli2002@gmail.com>
2021-08-19 08:00:01 -0700

    add initial argparse

--- ml/main.py
+++ ml/main.py
@@ -1,1 +1,14 @@
 import argparse
+
+parser = argparse.ArgumentParser(description='CLI for handling ML operations')
+
+parser.add_argument('mode', metavar='mode', type=str, help='mode with which to run')
+
+args = parser.parse_args()
+
+if args.mode == 'train_cnn':
+    print('training cnn mode')
+elif args.mode == 'train_clip':
+    print('training clip mode')
+else:
+    print('mode not recognized')
