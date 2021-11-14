f96e6190e01bce07a3ea85273e0ddf9f32691dc8
Ryan Tabrizi <rtabrizi@berkeley.edu>
2021-11-13 17:29:45 -0800

    made changes to matrix multiplication

--- ml/trainer/clip_mcoco_baseline.py
+++ ml/trainer/clip_mcoco_baseline.py
@@ -17,6 +17,14 @@ def train_clip_mcoco_baseline(epochs, batch_size, learning_rate):
     distilbert = DistilBertModel.from_pretrained('distilbert-base-uncased')
     vision_encoder = ResNet50(num_classes = 768)
     dataset_loader = get_cococaptions_dataloader(train=True, batch_size=batch_size)
+    wandb.init(project='mcoco_baseline', entity='phodexrdev')
+    config = wandb.config
+    config.update({
+        "num_epochs": epochs,
+        "batch_size": batch_size,
+        "learning_rate": learning_rate
+    })
+
 
     # freeze distilbert weights
     for param in distilbert.parameters():
@@ -42,14 +50,33 @@ def train_clip_mcoco_baseline(epochs, batch_size, learning_rate):
             # we want the CLS token which is the first token in all the sequences
             text_embeddings = text_embeddings_full.last_hidden_state[:,0,:]
             image_embeddings = vision_encoder(inputs)
+            # print(text_embeddings.shape, image_embeddings.shape)
+
+            image_embeddings = torch.transpose(image_embeddings, 0, 1)
+            #print(text_embeddings.shape, image_embeddings.shape)
+            t_log = nn.Parameter(torch.ones(1), requires_grad = True)
+            logits = torch.matmul(text_embeddings, image_embeddings) * torch.exp(t_log)
+            #print(logits.shape)
+
+            #cross entropy loss
+            labels = torch.arange(logits.shape[0])
+
             
+            loss = nn.CrossEntropyLoss()
+            loss1 = loss(logits,labels)
+            loss2 = loss(torch.transpose(logits,0,1), labels)
+            net_loss = (loss1 + loss2)/2
+            wandb.log({ 'epoch': epoch + 1, 'loss': net_loss})
+            #print(loss1, loss2)
+            #print(net_loss)
 
-            text_embeddings = torch.unsqueeze(text_embeddings, 1)
-            image_embeddings = torch.unsqueeze(image_embeddings, 0)
-            logits = torch.matmul(text_embeddings, image_embeddings)
-            print(logits.shape)
 
-            break
-        break
+
+
+
+
+
+            #break
+        #break
 
     print('Finished Training CLIP Baseline')
